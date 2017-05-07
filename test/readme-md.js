7503c4279302a72c0b5f079a2643742290a92e27
James Benner <james.benner@gmail.com>
2017-05-07 18:07:37 -0400

    Expanded code coverage.

--- test/readme-md.js
+++ test/readme-md.js
@@ -2,6 +2,7 @@
 
 const expect = require('chai').expect;
 const readme = require('../');
+const stripIndents = require('common-tags').stripIndents;
 
 describe('readme-md', function () {
     beforeEach(function () {
@@ -12,6 +13,52 @@ describe('readme-md', function () {
 
     it('is a function', function () {
         expect(readme).to.be.a('function');
+    });
+
+    it('generates a README with placeholders if not passed any arguments', function () {
+        let fixture = stripIndents`
+            &lt;package name&gt;
+            ====================
+            _To be documented._
+
+            Usage
+            -----
+            _To be documented._
+
+            Testing
+            -------
+            _To be documented._
+
+            License
+            -------
+            _To be documented._
+        `;
+
+        expect(readme()).to.equal(fixture);
+    });
+
+    it('generates a titled README with placeholders if passed an object with only a "name" property', function () {
+        let pkg = {name: 'awesome-package'};
+
+        let fixture = stripIndents`
+            awesome-package
+            ===============
+            _To be documented._
+
+            Usage
+            -----
+            _To be documented._
+
+            Testing
+            -------
+            _To be documented._
+
+            License
+            -------
+            _To be documented._
+        `;
+
+        expect(readme(pkg)).to.equal(fixture);
     });
 
     it('returns a string', function () {
